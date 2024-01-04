4b1f771acb2b9df900707d05de1c3f5b09575718
Sjonnie Boonstra <sjonnie.boonstra@epfl.ch>
2024-01-04 11:26:02 +0100

    added tryout test

--- hofss/src/task.py
+++ hofss/src/task.py
@@ -3,4 +3,9 @@ from ..data_structures import Actor, Scenario, Parameter, Organization, TaskType
 
 class Task:
 
+    def greet(self, name: str = None) -> str:
+        if name is None:
+            name = "world"
+        return f"Hello {name}!"
+
     pass
