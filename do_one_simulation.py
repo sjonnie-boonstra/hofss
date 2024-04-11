1ab7ef267e53ca291db4fb2cbc0603681ce1534c
SJONNIE <SJONNIE@SJONNIE.com>
2024-04-11 16:17:33 +0200

    postprocessing and plots

--- do_one_simulation.py
+++ do_one_simulation.py
@@ -6,8 +6,8 @@ from hofss import Simulator
 # input
 input_directory = "data"
 output_directory = "/home/boonstra/xin_results_23_3"
-number_of_parameter_draws = 1e6
-parameter_draw_batch_size = 1e6
+number_of_parameter_draws = 1e8
+parameter_draw_batch_size = 1e7
 
 # preparation
 simulator = Simulator.parse_from_directory(input_directory)
@@ -17,8 +17,9 @@ initial_failure_probabilities = simulator.structure.calculate_failure_probabilit
     number_of_parameter_draws, parameter_draw_batch_size
 )
 
-simulation_data = simulator.simulate(
-    86, number_of_parameter_draws, parameter_draw_batch_size, initial_failure_probabilities
-)
+print(initial_failure_probabilities)
+# simulation_data = simulator.simulate(
+#     86, number_of_parameter_draws, parameter_draw_batch_size, initial_failure_probabilities
+# )
 
-print(simulation_data)
+# print(simulation_data["total"][-1])
